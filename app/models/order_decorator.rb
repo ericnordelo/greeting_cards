5ff610d51b7a3a143819b916dae1a450ba996677
Eric Nordelo <eric.nordelo39@gmail.com>
2019-06-17 14:39:27 -0400

    Adding free orders in subscription

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -1,13 +1,30 @@
 Spree::Order.class_eval do
-    remove_checkout_step :address
-    remove_checkout_step :delivery
-    insert_checkout_step :select_destinatary, before: :payment
+    checkout_flow do
+        go_to_state :select_destinatary
+        go_to_state :payment, if: ->(order) {
+          order.update_totals
+          order.payment_required?
+        }
+        go_to_state :complete
+    end
+    # remove_checkout_step :address
+    # remove_checkout_step :delivery
+    # insert_checkout_step :select_destinatary, before: :payment
 
     Spree::Order.state_machine.after_transition to: :complete, do: :send_email
+
+    def payment_required?
+        self.user.free_cards == 0
+    end
 
     private
 
     def send_email
+        current = 
+        if current > 0
+            self.user.update_attribute :free_cards, current-1
+        end
         ProductsMailer.send_product(self.destinatary_email, self.products).deliver
     end
+
 end
